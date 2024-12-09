f63eeab55d63655694e334e0cd8c078352fa46a2
Levi Heßmann <levihsmn@gmail.com>
2024-12-09 20:40:18 +0100

    fix: Lombok

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,4 +40,7 @@ dependencies {
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+
+    compileOnly("org.projectlombok:lombok:1.18.34")
+    annotationProcessor("org.projectlombok:lombok:1.18.34")
 }
